d9a30f3a08509feac82e84df083fc82cc1d38d3a
Peter Oyebanji <peter@prob.ca>
2020-11-12 11:35:41 -0700

    signin-register

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require ('express');
 const bodyParser = require ('body-parser')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
 const app =   express()
 
 
@@ -20,21 +25,39 @@ app.get('/', (req,res)=>{
 
 app.post('/register', (req,res)=>{
     console.log(req.body)
-    const {password,id,name} = req.body
+    const {password,hobby,name} = req.body
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        console.log(hash)
+        // Store hash in your password DB.
+        database.users.push({
+            name:name,
+            password:hash,
+            hobby: hobby,
+            joined: new Date()    
+        })
+        res.json(hobby)
 
-    database.users.push({
-        id:id,
-        name:name,
-        password:password
-
-    })
-
-    res.json('user added successfully')
-
+    });
+    
 })
 
-app.get('/about', (req,res)=>{
-    res.send("<h1>I am About </h1>")
+app.post('/signin', (req,res)=>{
+    const { name, password} = req.body
+    let found = false
+    database.users.forEach((user)=> {
+if (name === user.name) {
+    const isFound = bcrypt.compareSync(password, user.password); 
+    found = isFound
+    if(found){
+        res.json(user)
+    }
+    
+}
+    })
+if (!found){
+    res.json("Bad Credentials")
+}
+
 })
 
 app.listen(3000,()=> {
